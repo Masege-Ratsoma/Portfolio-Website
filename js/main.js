630525ca4a4d981bf5830d8b14986e092e152fe0
Masege Ratsoma <dynamitegod2@gmail.com>
2025-02-26 20:44:03 +0200

    deleted index.js, changed to main.js to run script in html class call. modified styles and index.html

--- js/main.js
+++ js/main.js
@@ -3,14 +3,14 @@
 
 const navMenu = document.getElementById('nav-menu'),
 navToggle = document.getElementById('nav-toggle'),
-navClose= document.getElementById(nav-close)
+navClose= document.getElementById('nav-close')
 
 
 /* check if menu shows */
-
+/* Validate if constant exists */
 if(navToggle){
-  navToggle.addEventListener('click', ()=>{
-      navMenu.classList.remove('show-menu')
+  navToggle.addEventListener('click', () =>{
+      navMenu.classList.add('show-menu')
   })
 }
 
