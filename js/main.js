04de2a5415f5112eba4fe02c38188c98a242cb0c
Masege Ratsoma <dynamitegod2@gmail.com>
2025-02-26 20:54:27 +0200

    modified index.html and main.js

--- js/main.js
+++ js/main.js
@@ -22,4 +22,15 @@ if(navToggle){
     })
  }
 
+ /* === remove menu mobile === */
+
+ const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
  
