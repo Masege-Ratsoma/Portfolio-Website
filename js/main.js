f330c77ef276734009cd4cb01d58f28a7ac9685e
Masege Ratsoma <dynamitegod2@gmail.com>
2025-02-28 12:47:56 +0200

    Added About And Skills, Updated Css and added code to main.js

--- js/main.js
+++ js/main.js
@@ -33,4 +33,12 @@ function linkAction(){
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
- 
+
+ /*=== Code for Blur to header ===*/
+
+const blurHeader = () =>{
+    const header = document.getElementById('header')
+    this.scrollY >= 50 ? header.classList.add('blur-header')
+                       : header.classList.remove('blur-header')
+}
+window.addEventListener('scroll', blurHeader)
